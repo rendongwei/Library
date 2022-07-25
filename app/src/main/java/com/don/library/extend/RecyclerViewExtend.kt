651e412ce5b10d5f95cb2c4b5ec892695ad7d6cc
rendongwei <86930007@qq.com>
2022-07-25 08:53:38 +0800

    添加griditemdecoration

--- app/src/main/java/com/don/library/extend/RecyclerViewExtend.kt
+++ app/src/main/java/com/don/library/extend/RecyclerViewExtend.kt
@@ -84,6 +84,26 @@ fun RecyclerView.addItemDecoration(listener: ((position: Int) -> Rect)): Recycle
     }
 }
 
+fun RecyclerView.addGridItemDecoration(
+    spanCount: Int,
+    includeEdge: Boolean,
+    spacing: Int,
+    verticalSpacing: Int = 0,
+    fromPosition: Int = 0
+): RecyclerView {
+    return apply {
+        addItemDecoration(
+            GridItemDecoration(
+                spanCount,
+                includeEdge,
+                spacing,
+                verticalSpacing,
+                fromPosition
+            )
+        )
+    }
+}
+
 class DividerItemDecoration constructor(@NonNull var mListener: ((position: Int) -> Rect)) :
     RecyclerView.ItemDecoration() {
     override fun getItemOffsets(
@@ -95,6 +115,44 @@ class DividerItemDecoration constructor(@NonNull var mListener: ((position: Int)
         var position = (view?.layoutParams as RecyclerView.LayoutParams).viewLayoutPosition
         var rect = mListener.invoke(position)
         outRect?.set(rect)
+    }
+}
+
+class GridItemDecoration(
+    private val spanCount: Int,
+    private val includeEdge: Boolean,
+    private val spacing: Int,
+    private val verticalSpacing: Int = 0,
+    private val fromPosition: Int = 0
+) :
+    RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        var position = parent.getChildAdapterPosition(view)
+        if (position < fromPosition) {
+            return
+        }
+
+        position -= fromPosition
+        val column = position % spanCount
+        if (includeEdge) {
+            outRect.left = spacing - column * spacing / spanCount
+            outRect.right = (column + 1) * spacing / spanCount
+            if (position < spanCount) {
+                outRect.top = if (verticalSpacing == 0) spacing else verticalSpacing
+            }
+            outRect.bottom = if (verticalSpacing == 0) spacing else verticalSpacing
+        } else {
+            outRect.left = column * spacing / spanCount
+            outRect.right = spacing - (column + 1) * spacing / spanCount
+            if (position >= spanCount) {
+                outRect.top = if (verticalSpacing == 0) spacing else verticalSpacing
+            }
+        }
     }
 }
 
