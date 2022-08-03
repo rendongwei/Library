82eb820814a4ef17d1a4152b38a8cbe1827b643c
rendongwei <86930007@qq.com>
2022-08-03 16:54:12 +0800

    修改MMKV 添加可设置默认返回值

--- app/src/main/java/com/don/library/extend/MMKVExtend.kt
+++ app/src/main/java/com/don/library/extend/MMKVExtend.kt
@@ -34,28 +34,28 @@ fun Any.removeFromMMKV(key: String) {
     MMKV.defaultMMKV()?.removeValueForKey(key)
 }
 
-inline fun <reified T> Any?.getFromMMKV(key: String): T? {
+inline fun <reified T> Any?.getFromMMKV(key: String, default: T? = null): T? {
     return when (T::class.simpleName) {
         Int::class.simpleName -> {
-            MMKV.defaultMMKV()?.getInt(key, 0) as? T
+            MMKV.defaultMMKV()?.getInt(key, (default as? Int) ?: 0) as? T
         }
         String::class.simpleName -> {
-            MMKV.defaultMMKV()?.getString(key, null) as? T
+            MMKV.defaultMMKV()?.getString(key, default?.toString()) as? T
         }
         Float::class.simpleName -> {
-            MMKV.defaultMMKV()?.getFloat(key, 0f) as? T
+            MMKV.defaultMMKV()?.getFloat(key, (default as? Float) ?: 0f) as? T
         }
         Long::class.simpleName -> {
-            MMKV.defaultMMKV()?.getLong(key, 0L) as? T
+            MMKV.defaultMMKV()?.getLong(key, (default as? Long) ?: 0L) as? T
         }
         Boolean::class.simpleName -> {
-            MMKV.defaultMMKV()?.getBoolean(key, false) as? T
+            MMKV.defaultMMKV()?.getBoolean(key, (default as? Boolean) ?: false) as? T
         }
         ByteArray::class.simpleName -> {
-            MMKV.defaultMMKV()?.getBytes(key, null) as? T
+            MMKV.defaultMMKV()?.getBytes(key, default as? ByteArray) as? T
         }
         else -> {
-            MMKV.defaultMMKV()?.getString(key, null).toBean() as? T
+            (MMKV.defaultMMKV()?.getString(key, null).toBean() as? T) ?: default
         }
     }
 }
